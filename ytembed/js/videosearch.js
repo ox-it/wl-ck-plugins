81dba90b78801cda8e73c8cc170a6a12c2c9e8d9
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-07-14 11:44:49 +0100

    VIDEOSEARCH.JS: INITIALIZE SETTINGS PROPERTY
    Set up the settings property. Main differences from ytsearch.js are:
    
    - A base url must be specified so that the service is abstracted from
      YouTube
    - For displayResult, a different object structure for the result is
      assumed (a simpler one). Will be noted in the documentation, but will
      probably just be title, description, id and meta to hold the rest

--- ytembed/js/videosearch.js
+++ ytembed/js/videosearch.js
@@ -3,6 +3,25 @@
   */
 (function($) {
   $.fn.videosearch = function(options) {
-    // code
+    /**
+      * PROPERTIES 
+      */
+    // initialize the settings
+    var settings = $.extend({
+      baseUrl: 'http://youtu.be/',
+      classes: {
+        query: '#query',
+        button: '#button',
+        results: '#searchresults',
+      },
+      params: {
+        order: 'relevance',
+        maxResults: '5',
+      },
+      noResults: '<p>Sorry, no results!</p>',
+      displayResult: function (result) {
+        return '<p><a href="' settings.baseUrl + result.id + '">' + item.title + '</p>';
+      },
+    }, options);
   };
 })(jQuery);
