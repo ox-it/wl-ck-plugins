fa14e6b3974b6df352d80914ad6b8417aed55e83
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-07-14 13:20:13 +0100

    VIDEOSEARCH-YOUTUBE.JS: INITIALIZE QUERY, BUILD URL, DO AJAX CALL
    Removed q parameter since the value for that field is provided by the
    first .each() loop

--- ytembed/js/videosearch-youtube.js
+++ ytembed/js/videosearch-youtube.js
@@ -12,11 +12,41 @@ var YouTubeSearch = function(options) {
       q: '#query',
     },
     params: {
-      q: '#query',
       order: 'relevance',
       maxResults: '5',
     },
   }, options);
+
+  // initialize the query
+  var query = settings.params;
+
+  // take values from existing fields (if the classes are defined)
+  $.each(settings.params, function(param, value) {
+    var setting = $(settings.classes[param]).val();
+    if (setting) query[param] = setting;
+  });
+
+  // get base Youtube API URL and build up the string
+  var url = 'https://www.googleapis.com/youtube/v3/search?key=' + settings.key + ' &part=snippet';
+
+  // build the string using the query property defined at the start
+  $.each(query, function(key, value) {
+    url = url + '&' + key + '=' + value;
+  });
+
+  // now perform the JSON call to get the results and format them
+  $.ajax({
+    url: url,
+    dataType: 'json',
+    success: function(json) {
+      if (json.items.length > 0) {
+        // format the results ...
+      }
+    },
+    error: function () {
+      return results;
+    }
+  });
 
   return results;
 };
