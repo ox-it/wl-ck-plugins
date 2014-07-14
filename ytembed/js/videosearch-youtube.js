162c7d0d562a9c8cc55fbd92543605ef68ee6103
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-07-14 13:10:24 +0100

    VIDEOSEARCH-YOUTUBE: ADD OPTIONS, INITIALIZE SETTINGS
    Options are the api key, the classes that correspond to any inputs in the
    form and default values for the parameters

--- ytembed/js/videosearch-youtube.js
+++ ytembed/js/videosearch-youtube.js
@@ -1,8 +1,22 @@
 /**
   * DOCUMENTATION
   */
-var YouTubeSearch = function() {
+var YouTubeSearch = function(options) {
+  // initialized results
   var results = [];
   
+  // merge default settings and options
+  var settings = $.extend({
+    key: 'YOURAPIKEY',
+    classes: {
+      q: '#query',
+    },
+    params: {
+      q: '#query',
+      order: 'relevance',
+      maxResults: '5',
+    },
+  }, options);
+
   return results;
 };
