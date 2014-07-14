9ad55707249a361bd0769dfaaa130b5e635e90b7
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-07-14 12:43:17 +0100

    VIDEOSEARCH.JS: ADD CLASSES TO PARAMS
    Need selectors for the results, query field and search button, so those
    are specified. Also, reference to this is saved for later

--- ytembed/js/videosearch.js
+++ ytembed/js/videosearch.js
@@ -3,11 +3,19 @@
   */
 (function($) {
 $.fn.videosearch = function(options) {
+  // this
+  var _this = this;
+
   // settings
   var settings = $.extend({
     callback: function() { // provides array of objects (the search results)
       return [];
     },
+    classes: {
+      results: '#results',
+      query: '#query',
+      button: '#button'
+    }
     displayResult(result): function() {
       return '<li><a href="' + result.url + '">' + result.title + '</li>';
     }
