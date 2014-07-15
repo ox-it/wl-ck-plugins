134011ceba3ed6318ff4602ef495dde14d504d27
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-07-15 11:48:46 +0100

    RENAME ALL TWITTER INSTANCES FOR RTT

--- researcher-training-tool/plugin.js
+++ researcher-training-tool/plugin.js
@@ -1,29 +1,29 @@
-CKEDITOR.plugins.add('twitter', {
-  icons: 'twitter',
+CKEDITOR.plugins.add('researcher-training-tool', {
+  icons: 'researcher-training-tool',
   init: function(editor) {
-    editor.addCommand('twitter', new CKEDITOR.dialogCommand('twitterDialog'));
-    editor.ui.addButton('twitter', {
-      label: 'Insert Twitter Timeline',
-      command: 'twitter',
+    editor.addCommand('researcher-training-tool', new CKEDITOR.dialogCommand('researcherTrainingToolDialog'));
+    editor.ui.addButton('researcher-training-tool', {
+      label: 'Insert Researcher Training Tool Listing',
+      command: 'researcher-training-tool',
       toolbar: 'insert'
     });
 
     if (editor.contextMenu) {
-      editor.addMenuGroup('twitterGroup');
-      editor.addMenuItem('twitterItem', {
-        label: 'Edit Timeline',
-        icon: this.path + 'icons/twitter.png',
-        command: 'twitter',
-        group: 'twitterGroup'
+      editor.addMenuGroup('researcherTrainingToolGroup');
+      editor.addMenuItem('researcherTrainingToolItem', {
+        label: 'Configure Courses',
+        icon: this.path + 'icons/researcher-training-tool.png',
+        command: 'researcher-training-tool',
+        group: 'researcherTrainingToolGroup'
       });
 
       editor.contextMenu.addListener(function(element) {
-        if (element.getAscendant('div', true) && element.hasClass('twitter-timeline')) {
-          return { twitterItem: CKEDITOR.TRISTATE_OFF };
+        if (element.getAscendant('div', true) && element.hasClass('courses-widget-container')) {
+          return { researcherTrainingToolItem: CKEDITOR.TRISTATE_OFF };
         }
       });
     }
 
-    CKEDITOR.dialog.add('twitterDialog', this.path + 'dialogs/twitter.js');
+    CKEDITOR.dialog.add('researcherTrainingToolDialog', this.path + 'dialogs/researcher-training-tool.js');
   }
 });
