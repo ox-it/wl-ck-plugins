f48b423820cff554afaae3ae4dbecba96f9e81d4
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-07-15 11:52:07 +0100

    REGISTER BASIC DIALOG

--- researcher-training-tool/dialogs/researcher-training-tool.js
+++ researcher-training-tool/dialogs/researcher-training-tool.js
@@ -3,5 +3,13 @@
 var h = CKEDITOR.plugins.get('researcher-training-tool');
 var path = h.path;
 
+// load css and javascript files
 // ...
+
+// register dialog
+CKEDITOR.dialog.add('researcherTrainingToolDialog', function(editor) {
+  return {
+    // dialog definition
+  };
+});
 })();
