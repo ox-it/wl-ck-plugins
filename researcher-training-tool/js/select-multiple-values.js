2f7756d53325d61a44a345ee17c897e1c6faac58
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-07-16 12:38:38 +0100

    ADD DOCUMENTATION TO API

--- researcher-training-tool/js/select-multiple-values.js
+++ researcher-training-tool/js/select-multiple-values.js
@@ -1,3 +1,9 @@
+/**
+  * This adds new methods to CKEditor's API for handling dialog ui select
+  * elements with multiple-select enabled
+  */
+
+// returns array of values selected from existing dialog element
 CKEDITOR.ui.dialog.uiElement.prototype.getValues = function() {
   // get the options from the dialog's input element
   var select = this.getInputElement();
@@ -12,13 +18,16 @@ CKEDITOR.ui.dialog.uiElement.prototype.getValues = function() {
   return values;
 };
 
+// highlights the values in the input array on the multiple-select field
+// @param values {array}
+//    Values to highlight on the multiple-select field
 CKEDITOR.ui.dialog.uiElement.prototype.setValues = function(values) {
   var select = this.getInputElement();
 
   // go through options; if that option is in the values array, set it
   for (i = 0; i < select.$.length; i++) {
     // first reset the option, otherwise if the dialog has been used
-    // multiple times, the options for various rtt embeds may bleed together
+    // multiple times, the options for various dialogs may bleed together
     select.$[i].selected = false;
 
     if (values.indexOf(select.$[i].value) > -1) {
