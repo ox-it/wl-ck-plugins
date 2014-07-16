2181758b2c3960d77418a00d609d85fef2da65dd
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-07-16 18:23:53 +0100

    FIX INDENTATION

--- youtube/plugin.js
+++ youtube/plugin.js
@@ -9,34 +9,33 @@
 CKEDITOR.plugins.add('youtube', {
   icons: 'youtube',
 
-	init: function(editor) {
-		editor.addCommand('youtube', new CKEDITOR.dialogCommand('youtubeDialog'));
-    editor.ui.addButton( 'Youtube', {
-      label: 'Embed Youtube Video',
-      command: 'youtube',
-      toolbar: 'insert'
-		});
+  init: function(editor) {
+    editor.addCommand('youtube', new CKEDITOR.dialogCommand('youtubeDialog'));
+  editor.ui.addButton( 'Youtube', {
+    label: 'Embed Youtube Video',
+    command: 'youtube',
+    toolbar: 'insert'
+    });
 
-    // add context menu for editing a video link
-		if (editor.contextMenu) {
-			editor.addMenuGroup('youtubeGroup');
-			
-			editor.addMenuItem( 'youtubeItem', {
-				label: 'Edit Video',
-				icon: this.path + 'icons/youtube.png',
-				command: 'youtube',
-				group: 'youtubeGroup'
-			});
+  // add context menu for editing a video link
+    if (editor.contextMenu) {
+      editor.addMenuGroup('youtubeGroup');
 
-			editor.contextMenu.addListener(function(element) {
-			  if (element.getAscendant('div', true) && element.hasAttribute('data-youtube-embed')) {
-					return { youtubeItem: CKEDITOR.TRISTATE_OFF };
-				}
-			});
-		}
+      editor.addMenuItem( 'youtubeItem', {
+        label: 'Edit Video',
+        icon: this.path + 'icons/youtube.png',
+        command: 'youtube',
+        group: 'youtubeGroup'
+      });
 
-		// register dialog
-		CKEDITOR.dialog.add( 'youtubeDialog', this.path + 'dialogs/youtube.js' );
-	}
+      editor.contextMenu.addListener(function(element) {
+        if (element.getAscendant('div', true) && element.hasAttribute('data-youtube-embed')) {
+          return { youtubeItem: CKEDITOR.TRISTATE_OFF };
+        }
+      });
+    }
+
+    // register dialog
+    CKEDITOR.dialog.add( 'youtubeDialog', this.path + 'dialogs/youtube.js' );
+  }
 });
-
