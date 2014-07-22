ff8e7584514155966d5cb4c75eaa3bfb270fbf98
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-07-22 17:03:50 +0100

    Add settings initialization and performQuery method

--- youtube/js/videosearch-youtube.js
+++ youtube/js/videosearch-youtube.js
@@ -14,5 +14,49 @@
 
   */
 var YouTubeSearchService = function(options) {
-  // ...
+  // initialize settings
+  var settings = $.extend({
+    key: 'YOURAPIKEY',
+    params: {
+      order: 'relevance',
+      maxResults: '5',
+    },
+  }, options);
+
+  // takes a string and returns array of objects representing each search
+  // result (i.e. {title, url, etc...})
+  var performQuery = function(searchTerm) {
+    var results = [];
+    var query = settings.query;
+    var url = 'https://www.googleapis.com/youtube/v3/search?key=' + settings.key + ' &part=snippet';
+
+    // build the string using the query property defined at the start
+    $.each(query, function(key, value) {
+      url = url + '&' + key + '=' + value;
+    });
+
+    url += '&q=' + searchTerm;
+
+    // now perform the JSON call to get the results and format them
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      async: false,
+      success: function(json) {
+        if (json.items.length > 0) {
+          // go through each result, formatting them for VideoSearch
+          $.each(json.items, function(key, item) {
+            results.push({
+              url: 'http://youtu.be/' + item.id.videoId,
+              title: item.snippet.title,
+              description: item.snippet.description,
+              meta: item.snippet
+            });
+          });
+        }
+      }
+    });
+
+    return results;
+  }
 };
