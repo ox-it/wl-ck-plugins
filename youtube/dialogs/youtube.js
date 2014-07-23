54d598bda56b9f7effe2886e691210fc8a98930b
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-07-23 10:03:24 +0100

    Massive changes to youtube.js dialog
    Basically edited to use videosearch.js and service.js instead of
    ytsearch.js

--- youtube/dialogs/youtube.js
+++ youtube/dialogs/youtube.js
@@ -6,17 +6,38 @@ var path = h.path;
 // load css and javascript files
 CKEDITOR.document.appendStyleSheet("http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css");
 CKEDITOR.document.appendStyleSheet(CKEDITOR.getUrl(path + "css/ytembed.css"));
+
+CKEDITOR.scriptLoader.load(path + '/js/videosearch.js');
+CKEDITOR.scriptLoader.load(path + '/js/service.js');
 CKEDITOR.scriptLoader.load(path + '/js/key.js');
-CKEDITOR.scriptLoader.load(path + '/js/ytsearch.js');
-CKEDITOR.scriptLoader.load(path + '/js/ytembed.js');
 
 var displaySearchPage = function() {
-  var h2 = $('<h2/>').html('Embed a video');
-  var p  = $('<p>').html('Type a search term or YouTube URL below, hit the search button, then select a result to embed that video.');
-  var form = $('<div/>').attr('id', 'youTubeSearchForm');
-  var results = $('<div/>').attr('id', 'youTubeSearchResults');
+  var header = $('<h2/>').html('Embed a video');
+  var explanation  = $('<p>').html('Type a search term or YouTube URL below, hit the search button, then select a result to embed that video.');
+  var searchResultId = $('<input/>').attr('type', 'hidden').attr('id', 'searchResultId');
+  var searchForm = $('<div/>').attr('id', 'youTubeSearchForm');
+  var searchResults = $('<div/>').attr('id', 'youTubeSearchResults');
 
-  return $('<div/>').append(h2).append(p).append(form).append(results);
+  return $('<div/>').append(header)
+                    .append(explanation)
+                    .append(searchResultId)
+                    .append(searchForm)
+                    .append(searchResults)
+                    .html();
+};
+
+var displaySearchResult = function(result) {
+  return '<li class="result" data-src="' + result.meta.id + '">' +
+            '<img class="thumbnail" src="' + result.meta.thumbnails.m + '">' +
+            '<h2>' + result.title + '</h2>' +
+            '<p>' + result.description + '</p>' +
+         '</li>';
+};
+
+var clickDialogOK = function() {
+  var ckDialog = window.CKEDITOR.dialog.getCurrent();
+  var ckOk = ckDialog._.buttons['ok'];
+  ckOk.click();
 };
 
 CKEDITOR.dialog.add('youtubeDialog', function(editor) {
@@ -28,136 +49,69 @@ CKEDITOR.dialog.add('youtubeDialog', function(editor) {
 
     contents: [
       {
-        // Definition of 'Search Youtube' tab
         id: 'tab-search',
         label: 'Search YouTube',
         elements : [
           {
             type: 'html',
             id: 'searchpage',
-            // below defines the basic markup for the dialog.
-            // because hitting the enter key closes the dialog, and we want
-            // hitting enter to perform the search query, we define an iframe
-            // to hold the form, then use jQuery to bind the events between
-            // the frame and the dialog.
-            html: '<iframe id="searchiframe" src="about:blank"></iframe>' +
-                  '<div id="searchiframecont">' +
-                    '<form id="searchform">' +
-                      '<h2>Embed a video</h2>' +
-                      '<p>Type a search term or YouTube URL below, hit the search button, then select a result to embed that video.</p>' + 
-                        '<div id="searchwrap">' + 
-                          '<input id="searchid" type="hidden"/>' +
-                          '<input id="searchquery" class="cke_dialog_ui_input_text" type="text" placeholder="Search here..."/>' +
-                          '<a id="searchbutton" class="cke_dialog_ui_button cke_dialog_ui_button_ok">&#xf002;</a>' +
-                        '</div>' +
-                    '</form>' + 
-                  '</div>' +
-                  '<ul id="searchresults"></ul>',
+            html: displaySearchPage(),
             onLoad: function() {
-              /**
-                * pseudocode for cleaned up version
-                */
-              var iframe = $('#youTubeDialog iframe');
-              var content = iframe.contents();
-              var form = $('#youTubeDialog form');
-              var div = $('#youTubeSearchForm');
+              YouTubeSearchService.pt.key = googleApiKey;
 
-              // bind search functionality to div
-              div.videosearch({
+              var container = $('#youTubeSearchForm');
+              var searchResults = $('#youTubeSearchResults');
+
+              container.videosearch({
                 service: YouTubeSearchService,
-                resultsContainer: $('#youTubeSearchResults')
+                resultsContainer: searchResults,
+                displayResult: displaySearchResult
               });
 
-              content.find('body').html(div);
-              });
+              var iframe = $('<iframe src="about:blank"></iframe>').attr('id', 'youTubeSearchIframe');
+              container.after(iframe);
 
-
-
-
-              // select the frame content
-              var content = $('#searchiframecont').show();
-              var searchiframe = $('#searchiframe').contents();
-
-              // dump the frame content into the iframe, putting in any header scripts and css
-              // also reset the body's css
-              searchiframe.find('body').append('<div id="searchiframecont">' + content.html() + '</div>');
-              searchiframe.find('head').append($('head script, head link').clone());
-              searchiframe.find('head').append(
-                '<script src="' + path + '/js/ytsearch.js"></script>' +
-                '<script src="' + path + '/js/ytembed.js"></script>'
-              );
-              
-              // reset the frame body's css and hide the original form
-              searchiframe.find('body').css({
-                padding: 0,
-                width: '100%',
-              });
-              content.hide();
-              
-              // now bind youtube search functionality to the search box from
-              // within the iframe
-              var framecont = searchiframe.find('#searchiframecont');
-              var form = framecont.find('#searchform');
-              var query = form.find('#searchquery');
-              var button = form.find('#searchbutton');
-              
-              searchiframe.find('#searchform').ytsearch({
-                apiKey: googleApiKey,
-                classes: {
-                  query:    query,                // bound to query inside the frame
-                  results:  $('#searchresults'),  // bound to results box outside the frame
-                  button:   button,               // bound to button inside the frame
-                },
-                params: {
-                  type: 'video',                  // excludes playlists and channels
-                },
-                noResults: '<p class="noresults">Sorry, no results.</p>',
-                displayResult: function(item) {
-                  return '<li class="result" data-src="' + item.id.videoId + '">' +
-                            '<img class="thumbnail" src="' + item.snippet.thumbnails.medium.url + '">' +
-                            '<h2>' + item.snippet.title + '</h2>' +
-                            '<p>' + item.snippet.description + '</p>' +
-                         '</li>';
-                }
-              });
-
-              // when enter key is hit, simulate clicking the search button
-              searchiframe.on('submit', '#searchform', function() {
-                $(this).find('#searchbutton').click();
-                return false;
+              iframe.load(function() {
+                var $this = $(this);
+                var contents = $this.contents();
+                contents.find('head').append($('head script, head link').clone());
+                contents.find('body').html(container);
+                contents.find('body').css({
+                  padding: 0,
+                  width: '100%',
+                });
               });
 
               // if you click on a search result, its id should populate the
               // search field and the OK button should be clicked
-              $('#searchresults').on('click', '.result', function() {
-                searchiframe.find('#searchid').val($(this).data('src'));
-                var ckDialog = window.CKEDITOR.dialog.getCurrent()
-                var ckOk = ckDialog._.buttons['ok'];
-                ckOk.click();
-                return false;
+              searchResults.on('click', '.result', function(e) {
+                $('#searchResultId').val($(this).data('src'));
+                clickDialogOK();
+                e.preventDefault();
               });
             },
             setup: function(element) {
-              // if we are editing a video, its src should populate the query
-              var frameForm = $('#searchiframe').contents().find('#searchiframecont');
-              frameForm.find('#searchquery').val(element.data('src'));
+              var iframe = $('#youTubeSearchIframe').contents();
+              var id = $('#searchResultId').val();
 
-              // simulate search so that first result is the edited video
-              frameForm.find('form').submit();
+              // populate the search field with existing video's id then submit
+              // the form so that the video is the first result
+              iframe.find('input').val(id);
+              iframe.find('form').submit();
             },
             commit: function (element) {
-              // saving changes
-              var framecontent = $('#searchiframe').contents().find('#searchiframecont');
+              // use the search result id if it has been set
+              var value = $('#searchResultId').val();
 
-              // use the searchid if it has been set
-              var value = framecontent.find('#searchid').val();
-              if (!value)
-                value = framecontent.find('#searchquery').val();
+              if (!value) {
+                value = $('#youTubeSearchIframe').contents().find('input').val();
+              }
 
-              if (value) 
+              if (value) {
                 element.setAttribute('data-src', value);
-              else if (!this.insertMode)
+              } else if (!this.insertMode) {
                 element.removeAttribute('data-src');
+              }
             }
           },
         ]
@@ -178,22 +132,24 @@ CKEDITOR.dialog.add('youtubeDialog', function(editor) {
         element = editor.document.createElement('div');
         element.data('youtube-embed', true);
         this.insertMode = true;
-      }
-      else
+      } else {
         this.insertMode = false;
+      }
 
       this.element = element;
 
-      if (!this.insertMode)
+      if (!this.insertMode) {
         this.setupContent(this.element);
+      }
     },
 
     onOk: function() {
       var dialog = this;
       var youtube = this.element;
       this.commitContent(youtube);
-      if (this.insertMode)
+      if (this.insertMode) {
         editor.insertElement(youtube);
+      }
     }
   };
 });
