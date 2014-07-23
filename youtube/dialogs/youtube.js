66f62baeeda3e1a2403a2ac34571fd973daf2962
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-07-23 11:38:16 +0100

    Move bindVideoSearchToContainer into separate file

--- youtube/dialogs/youtube.js
+++ youtube/dialogs/youtube.js
@@ -11,6 +11,7 @@ CKEDITOR.scriptLoader.load(path + '/js/videosearch.js');
 CKEDITOR.scriptLoader.load(path + '/js/service.js');
 CKEDITOR.scriptLoader.load(path + '/js/key.js');
 CKEDITOR.scriptLoader.load(path + '/js/result.js');
+CKEDITOR.scriptLoader.load(path + '/js/bind-videosearch-to-container.js');
 
 var displaySearchPage = function() {
   var div = $('<div/>');
@@ -25,48 +26,6 @@ var displaySearchPage = function() {
   });
 
   return div.html();
-};
-
-var bindVideoSearchToContainer = function(container, searchResults, result) {
-  // initial binding
-  container.videosearch({
-    service: YouTubeSearchService,
-    resultsContainer: searchResults,
-    displayResult: result.display,
-  });
-
-  // now initialize iframe which will isolate the search field (so we can submit
-  // the form without closing the dialog
-  var iframe = $('<iframe src="about:blank"></iframe>').attr('id', 'youTubeSearchIframe');
-  container.after(iframe);
-
-  // put form into iframe and fix the internal css styling
-  iframe.load(function() {
-    var contents = $(this).contents();
-
-    contents.find('head').append($('head script, head link').clone());
-    contents.find('body').html(container);
-    contents.find('body').css({ padding: 0, width: '100%' });
-    contents.find('input').addClass('searchQuery cke_dialog_ui_input_text')
-                          .attr('placeholder', 'Search here...');
-    contents.find('a').addClass('searchButton cke_dialog_ui_button cke_dialog_ui_button_ok')
-                      .html('&#xf002;');
-  });
-
-  // if you click on a search result, its id should populate the
-  // search field and the OK button should be clicked
-  searchResults.on('click', '.result', function(e) {
-    $('#searchResultId').val($(this).data('src'));
-    clickDialogOK();
-    e.preventDefault();
-  });
-};
-
-// method for easily confirming the dialog
-var clickDialogOK = function() {
-  var ckDialog = window.CKEDITOR.dialog.getCurrent();
-  var ckOk = ckDialog._.buttons['ok'];
-  ckOk.click();
 };
 
 CKEDITOR.dialog.add('youtubeDialog', function(editor) {
