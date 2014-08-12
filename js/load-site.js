e51af67d9f18a4e9e7ba73bfe279610ca561a78a
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-08-12 13:16:54 +0100

    Move javascript into separate file
    Also load pages all in one go and use anchors to go to each page

---
+++ js/load-site.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  // load all of the webpages
+  var pageLinks = $('#menu .pages a'); console.log(pageLinks);
+  var $content = $('.content');
+
+  pageLinks.each(function() {
+    var $this = $(this);
+    var page = $this.attr('href').replace('#', '') + '.html';
+    var $title = $('.header h2');
+    var $page = $('<div/>').addClass('page');
+
+    $.ajax({
+      url: page,
+      dataType: 'html',
+      success: function(html) {
+        var $html = $('<div/ >').append($(html));
+        var title = $html.find('#title').html();
+        var content = $html.find('#content').html();
+        var h2 = $('<h2/>').html(title);
+        var pageContent = $('<div/>').html(content);
+        var a = $('<a/>').attr('name', $this.attr('href').replace('#', ''));
+
+        $page.append(a).append(h2).append(pageContent);
+      },
+      error: function() {
+      },
+      complete: function() {
+        $content.append($page);
+
+        return false;
+      }
+    });
+
+    $this.on('click', function() {
+      
+    });
+  });
+});
