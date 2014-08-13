f871dcd86e8d6682bd0382a85924cefeb89114fc
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-08-13 10:07:09 +0100

    Fix jQuery handling of the site scrolling

--- js/load-site.js
+++ js/load-site.js
@@ -1,20 +1,80 @@
+/**
+  * jQuery for handling the loading of the site and behaviour of the links
+  */
 $(document).ready(function() {
-  // load all of the webpages
+  /**
+    * VARIABLES
+    */
   var pageLinks = $('#menu .pages a');
   var $content = $('.content');
 
-  // credit to Scott Dowding @ StackOverflow
-  // http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling
-  var isScrolledIntoView = function(elem) {
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
+  /**
+    * FUNCTIONS
+    */
 
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
-    console.log([docViewTop, docViewBottom, elemTop, elemBottom]);
-
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  var setCurrentPageLink = function($pageLinks, $link) {
+    $pageLinks.closest('li').removeClass('active');
+    $link.closest('li').addClass('active');
   };
+
+  var loadAjaxPageContent = function($page, ajaxHtml, pageId) {
+    var $html = $('<div/ >').append($(ajaxHtml));
+    var title = $html.find('#title').html();
+    var content = $html.find('#content').html();
+    var h2 = $('<h2/>').addClass('content-subhead').html(title);
+    var pageContent = $('<div/>').html(content);
+    var a = $('<a/>').attr('name', pageId);
+
+    $page.append(a).append(h2).append(pageContent);
+  };
+
+  var historyPushState = function(pageId) {
+    history.pushState(null, $('title').text(), '#' + pageId);
+  };
+
+  var bindWaypointToPages = function($pages, $pageLinks) {
+    var setUpPage = function($page) {
+      var page = $page.data('page');
+      var link = $('#menu a[href="#' + page + '"]');
+      var title = $page.find('h2').first().text();
+      var titlePrefix = $('.header h1').text();
+
+      setCurrentPageLink($pageLinks, link);
+      $('title').html(titlePrefix + ' - ' + title);
+    };
+
+    $pages
+      .waypoint(function(direction) {
+        if (direction === 'down') {
+          setUpPage($(this));
+        }
+      })
+      .waypoint(function(direction) {
+        if (direction === 'up') {
+          setUpPage($(this));
+        }
+      }, {
+        offset: function() {
+          return -$(this).height();
+        }
+      });
+  };
+
+  var bindScrollToPage = function($anchor, pageId) {
+    $anchor.on('click', function() {
+      $('html, body').animate({
+        scrollTop: $('[data-page="' + pageId + '"]').offset().top + 3,
+      }, 1000);
+
+      historyPushState(pageId);
+
+      return false;
+    });
+  }
+
+  /**
+    * PROCEDURES
+    */
 
   pageLinks.each(function(i, p) {
     var $this = $(this);
@@ -23,45 +83,27 @@ $(document).ready(function() {
     var $title = $('.header h2');
     var $page = $('<div/>').addClass('page').attr('data-page', id);
 
+    // load the page content
     $.ajax({
       url: page,
       dataType: 'html',
       async: false,
       success: function(html) {
-        var $html = $('<div/ >').append($(html));
-        var title = $html.find('#title').html();
-        var content = $html.find('#content').html();
-        var h2 = $('<h2/>').addClass('content-subhead').html(title);
-        var pageContent = $('<div/>').html(content);
-        var a = $('<a/>').attr('name', id);
-
-        $page.append(a).append(h2).append(pageContent);
+        loadAjaxPageContent($page, html, id);
       },
       error: function() {
       },
       complete: function() {
         $content.append($page);
 
-
+        // once all pages are loaded, bind waypoint() to them
         if (i == (pageLinks.length - 1)) {
           var pages = $('.page');
-          pages.waypoint(function(direction) {
-            var $this = $(this);
-            var page = $this.data('page');
-
-            var link = $('#menu a[href="#' + page + '"]');
-            pageLinks.closest('li').removeClass('active');
-            link.closest('li').addClass('active');
-          });
+          bindWaypointToPages(pages, pageLinks);
         }
-
-        return false;
       }
     });
 
-    $this.on('click', function() {
-      pageLinks.closest('li').removeClass('active');
-      $this.closest('li').addClass('active');
-    });
+    bindScrollToPage($this, id);
   });
 });
