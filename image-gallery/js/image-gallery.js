74f76cc1c2979d1614b5fbe22f97eebdd4428f31
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-08-14 11:48:26 +0100

    Remove console logging

--- image-gallery/js/image-gallery.js
+++ image-gallery/js/image-gallery.js
@@ -114,9 +114,7 @@ $.fn.wlImageGallery = function(options) {
           addImageToGallery(image, html);
         }
 
-        console.log(html);
         if (html.children().length > 0) {
-          console.log(html.children());
           var params = getSettingsFromContainer(i, $container);
           html.find('a').colorbox(params);
 
