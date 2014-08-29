2e3d5c194be480422749833e98cda14b801fd251
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-08-29 10:11:15 +0100

    Allow jQuery version to be defined in editor.config

--- jquery/plugin.js
+++ jquery/plugin.js
@@ -1,18 +1,15 @@
 (function() {
-/* properties */
-var jQueryVersion = 'jquery-1.11.1.js';
-
 /* methods */
 var getEditorDataJQuery = function(editor) {
   return $('<div>').append(editor.getData());
 };
 
-var embedJQueryInEditor = function(editor) {
+var embedJQueryInEditor = function(editor, version) {
   var $data = getEditorDataJQuery(editor);
-  var scripts = $data.find("script[src*='https://code.jquery.com/" + jQueryVersion + "']").remove();
+  var scripts = $data.find("script[src*='https://code.jquery.com/" + version + "']").remove();
   var script = $('<script/>').attr({
     type : 'text/javascript',
-    src: 'https://code.jquery.com/' + jQueryVersion,
+    src: 'https://code.jquery.com/' + version,
   });
 
   $data.prepend(script);
@@ -28,7 +25,10 @@ CKEDITOR.plugins.add('jquery', {
   init: function(editor) {
     editor.addCommand('jquery', {
       exec: function(editor) {
-        embedJQueryInEditor(editor);
+        console.log(editor.config);
+        console.log(editor.config.jQueryVersion);
+        var version = 'jquery-' + (editor.config.jQueryVersion || '1.11.1') + '.js';
+        embedJQueryInEditor(editor, version);
       },
     });
     editor.ui.addButton('jquery', { label: 'Embed jQuery Library', command: 'jquery', toolbar: 'insert' });
