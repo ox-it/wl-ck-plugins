81d4d65afca806e29e1ec8152f75bce54ddcbc8a
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-12-12 16:34:52 +0000

    Ensure oxam loads jquery dependencies

--- oxam/dialogs/oxam.js
+++ oxam/dialogs/oxam.js
@@ -10,6 +10,7 @@ CKEDITOR.document.appendStyleSheet(CKEDITOR.getUrl(path + 'css/dialog.css'));
 CKEDITOR.document.appendStyleSheet(CKEDITOR.getUrl(path + 'css/results.css'));
 
 CKEDITOR.scriptLoader.load(pathCommon + 'js/embed-assets-in-editor.js');
+CKEDITOR.scriptLoader.load(pathCommonWl + 'js/embed-jquery-assets-in-editor.js');
 CKEDITOR.scriptLoader.load(path + 'js/oxam-embed.js');
 CKEDITOR.scriptLoader.load(path + 'js/get-oxam-data.js');
 CKEDITOR.scriptLoader.load(path + 'js/get-html.js');
