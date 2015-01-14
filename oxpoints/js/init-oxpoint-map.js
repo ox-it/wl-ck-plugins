50fe357833b720ed52af7f69685af93dd439dde8
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2015-01-14 15:31:09 +0000

    Fix url path for loading gomap and oxpoints map in oxpoints

--- oxpoints/js/init-oxpoint-map.js
+++ oxpoints/js/init-oxpoint-map.js
@@ -4,18 +4,21 @@ var initOxPointMapPath = (function(){
 
   // find current scripts and copy the url (without the current filename)
   $('script').each(function(){
+    /*
     var src = $(this).attr('src');
     if (src.indexOf('init-oxpoint-map.js') > -1) {
       path = src.replace('init-oxpoint-map.js', '');
     }
+    */
   });
 
   return path;
 })();
 
 function initOxPointMap() {
-  var gomap = initOxPointMapPath + 'gomap.js';
-  var oxpoints = initOxPointMapPath + 'oxpoints-map.js';
+  var path = '/library/src/webapp/editor/ckextraplugins/oxpoints/js/';
+  var gomap = path + 'gomap.js';
+  var oxpoints = path + 'oxpoints-map.js';
 
   // load gomap, then oxpoints
   $.getScript(gomap).done(function() {
